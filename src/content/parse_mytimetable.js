3598ab3e278d3a9b6eadd5f95f3a06f47a2524f0
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2025-07-24 13:19:11 -0400

    added better error handling on edge cases

--- src/content/parse_mytimetable.js
+++ src/content/parse_mytimetable.js
@@ -1,68 +1,70 @@
+function legendTimeToRecur(legTime, year) {
+  const [days, time] = legTime
+    .split(/:\s+/)
+    .map((t) => t.replace(/[\t\r\n\f\v]/g, ' ').trim());
+  let begin, end;
+
+  const dayArray = days.split(',').map((d) => d.trim().toUpperCase());
+
+  // only the case if begin + end is included
+  if (dayArray.length !== days.split(/\s+/).length) {
+    const lastElem = dayArray[dayArray.length - 1];
+
+    // if begin + end is included, lastElem looks something like 'THU MAY 5 - JUN 13'
+    // the string is sliced to separate the dayArray elem from the dates
+    const [elem, dates] = [lastElem.slice(0, 3), lastElem.slice(4)];
+    dayArray[dayArray.length - 1] = elem;
+
+    begin = new Date(`${dates.split(' - ')[0]} ${year}`);
+    end = new Date(`${dates.split(' - ')[1]} ${year}`);
+  }
+
+  const times = time.split(/\s+to\s+/);
+
+  return {
+    freq: {
+      days: dayArray.map((d) => d.slice(0, 2)),
+      startTime: times[0],
+      endTime: times[1],
+    },
+    ...(begin && end && { begin, end }),
+  };
+}
+
+function parseRooms(roomElem) {
+  let rooms = [];
+
+  if (roomElem.querySelector('.legend_multi_loc')) {
+    rooms = Array.from(roomElem.querySelectorAll('.legend_multi_loc')).map(
+      (r) =>
+        r.textContent
+          .split('-')[1]
+          .replace(/\s+/, ' ')
+          .trim()
+          .replace('_', ' '),
+    );
+  } else if (roomElem.textContent.length > 0) {
+    for (let i = 0; i < 3; i++) {
+      // in case a particular classType has multiple timings but in the same room
+      rooms.push(
+        roomElem.textContent
+          .split('-')[1]
+          .replace(/\s+/, ' ')
+          .trim()
+          .replace('_', ' '),
+      );
+    }
+  }
+
+  return rooms.length > 0 ? rooms : ['', '', ''];
+}
+
 export default function parse() {
   const courseBoxes = document.querySelectorAll(
     '#legend_box>.course_box .course_cell_legend',
   );
 
-  function legendTimeToRecur(legTime, year) {
-    const [days, time] = legTime
-      .split(/:\s+/)
-      .map((t) => t.replace(/[\t\r\n\f\v]/g, ' ').trim());
-    let begin, end;
-
-    const dayArray = days.split(',').map((d) => d.trim().toUpperCase());
-
-    // only the case if begin + end is included
-    if (dayArray.length !== days.split(/\s+/).length) {
-      const lastElem = dayArray[dayArray.length - 1];
-
-      // if begin + end is included, lastElem looks something like 'THU MAY 5 - JUN 13'
-      // the string is sliced to separate the dayArray elem from the dates
-      const [elem, dates] = [lastElem.slice(0, 3), lastElem.slice(4)];
-      dayArray[dayArray.length - 1] = elem;
-
-      begin = new Date(`${dates.split(' - ')[0]} ${year}`);
-      end = new Date(`${dates.split(' - ')[1]} ${year}`);
-    }
-
-    const times = time.split(/\s+to\s+/);
-
-    return {
-      freq: {
-        days: dayArray.map((d) => d.slice(0, 2)),
-        startTime: times[0],
-        endTime: times[1],
-      },
-      ...(begin && end && { begin, end }),
-    };
-  }
-
-  function parseRooms(roomElem) {
-    let rooms = [];
-
-    if (roomElem.querySelector('.legend_multi_loc')) {
-      rooms = Array.from(roomElem.querySelectorAll('.legend_multi_loc')).map(
-        (r) =>
-          r.textContent
-            .split('-')[1]
-            .replace(/\s+/, ' ')
-            .trim()
-            .replace('_', ' '),
-      );
-    } else {
-      for (let i = 0; i < 3; i++) {
-        // in case a particular classType has multiple timings but in the same room
-        rooms.push(
-          roomElem.textContent
-            .split('-')[1]
-            .replace(/\s+/, ' ')
-            .trim()
-            .replace('_', ' '),
-        );
-      }
-    }
-
-    return rooms;
-  }
+  if (courseBoxes.length === 0) throw new Error('No courses selected.');
 
   let data = {
     sem: '',
@@ -128,8 +130,8 @@ export default function parse() {
 
       times.forEach((t, ti) =>
         data.classes.push({
-          begin,
-          end,
+          begin: new Date(begin),
+          end: new Date(end),
           name: courseTitle.textContent.replace(/\s+/g, ' ').trim(),
           classType,
           classSec,
