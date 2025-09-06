6d0559245c2df61e1ed916a288c124aa2f57d64e
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2025-09-06 12:05:12 -0400

    v1.0.6 - fixed parsing for courses with multiple room locations per class

--- src/content/parse_mytimetable.js
+++ src/content/parse_mytimetable.js
@@ -1,7 +1,5 @@
 function legendTimeToRecur(legTime, year) {
-  const [days, time] = legTime
-    .split(/:\s+/)
-    .map((t) => t.replace(/[\t\r\n\f\v]/g, ' ').trim());
+  const [days, time] = legTime.split(' : ').map((t) => t.trim());
   let begin, end;
 
   const dayArray = days.split(',').map((d) => d.trim().toUpperCase());
@@ -31,21 +29,57 @@ function legendTimeToRecur(legTime, year) {
   };
 }
 
-function parseRooms(roomElem) {
+function parseRooms(roomElem, roomFreqLength) {
   if (roomElem.querySelector('.legend_multi_loc')) {
-    return Array.from(
-      roomElem.querySelectorAll(roomElem.querySelector('.legend_multi_loc')),
-    ).map((r) =>
-      r.textContent.split('-')[1].replace(/\s+/, ' ').trim().replace('_', ' '),
-    );
+    // gets just the weekdays from each legend_multi_loc in a subarray per legend_multi_loc
+    const byDays = roomElem.innerHTML
+      .split('<br>')
+      .slice(0, -1)
+      .map((d) => d.split('<')[0].trim().split(':')[0].trim().split(', '));
+
+    // gets just the separate rooms for each legend_multi_loc
+    const byRooms = Array.from(
+      roomElem.querySelectorAll('.legend_multi_loc'),
+    ).map((r) => r.textContent.split('-')[1].trim().replace('_', ' '));
+
+    const weekFormat = Array(5).fill(''); // we'll put the room into its corresponding slot in this list based in the day it occurs
+
+    for (let i = 0; i < byDays.length; i++) {
+      const days = byDays[i].map((d) => d.toUpperCase());
+
+      for (const d of days) {
+        switch (d) {
+          case 'MON':
+            weekFormat[0] = byRooms[i];
+            break;
+          case 'TUE':
+            weekFormat[1] = byRooms[i];
+            break;
+          case 'WED':
+            weekFormat[2] = byRooms[i];
+            break;
+          case 'THU':
+            weekFormat[3] = byRooms[i];
+            break;
+          case 'FRI':
+            weekFormat[4] = byRooms[i];
+            break;
+
+          default:
+            break;
+        }
+      }
+    }
+
+    return weekFormat.filter((room) => room.length > 0);
   } else if (roomElem.textContent.length > 0) {
-    return [
+    return Array(roomFreqLength).fill(
       roomElem.textContent
         .split('-')[1]
         .replace(/\s+/, ' ')
         .trim()
         .replace('_', ' '),
-    ];
+    );
   }
 
   return [''];
@@ -71,9 +105,11 @@ export default function parse() {
       .split(':')
       .map((s) => s.replace(/\s+/g, ' ').trim());
 
-    const [begin, end] = dates // course start and end dates in the format `Month D YY`
+    let [begin, end] = dates // course start and end dates in the format `Month D YY 1:00 A.M.`
       .split(/\s+-\s+/)
       .map((d) => new Date(`${d} ${sem.slice(0, 4)}`));
+
+    end = end.setDate(end.getDate() + 1);
 
     data.sem = sem.toLowerCase().replace('/', '');
 
@@ -115,7 +151,16 @@ export default function parse() {
         // and that there's a case that 3 days show up in one line
         // i haven't seen what that would look like and how i should account for that
         // soooooooo
-        classTimesOrder = days.split(', ').map((d) => [`${d} : ${times}`]);
+        classTimesOrder = [
+          days
+            .split(', ')
+            .slice(0, -1)
+            .map((d) => `${d} : ${times}`),
+          days
+            .split(', ')
+            .slice(-1)
+            .map((d) => `${d} : ${times}`),
+        ];
       } else {
         const split =
           classOrder[0].includes('LEC') &&
@@ -141,32 +186,52 @@ export default function parse() {
       ];
     }
 
+    classTimesOrder = classTimesOrder.map((times) =>
+      times.map((t) => {
+        console.log(t);
+        const [days, times] = t.split(' : ');
+
+        console.log(days.split(', ').map((d) => `${d} : ${times}`));
+        return days.split(', ').map((d) => `${d} : ${times}`);
+      }),
+    );
+
     const roomOrder = Array.from(
       selectedClasses.querySelectorAll('.location_block'),
-    ).map((elem, i) => Array(classTimesOrder[i].length).fill(parseRooms(elem)));
+    ).map((elem, i) =>
+      classTimesOrder[i].map((t) => parseRooms(elem, t.length)),
+    );
 
-    console.log(courseTitle.textContent, roomOrder);
+    console.log(courseTitle.textContent, classTimesOrder, roomOrder);
 
     classTimesOrder.forEach((times, i) => {
       const [classType, classSec] = classOrder[i].split(' ');
       const [year, _] = data.sem.split(' ');
 
-      times.forEach((t, ti) =>
-        data.classes.push({
-          begin: new Date(begin),
-          end: new Date(end),
-          name: courseTitle.textContent.replace(/\s+/g, ' ').trim(),
-          classType,
-          classSec,
-          prof: profOrder[i],
-          loc: roomOrder[i][ti], // every time has a corresponding room
-          ...legendTimeToRecur(t, Number(year)),
-          // legendTimeToRecur returns an object of type { freq: { ... }, begin?: Date, end?: Date }
-        }),
-      );
+      // TODO: i've fixed the legend_multi_loc parsing, im just running into the issue now where
+      // a) i need the location to update properly, meaning i need to change up the parsing a bit
+      //    (i figure i would need to instead use roomOrder[i].forEach(), since i need to push each separate location to data)
+      //    yeah i think that's fine, i would have to give up some recurrence rules that make things neat but
+      //    that's fine, it just needs to look good
+      // that just needs me to update classTimesOrder honestly to separate by day
+
+      times.forEach((t, ti) => {
+        t.forEach((tblock, tbi) =>
+          data.classes.push({
+            begin: new Date(begin),
+            end: new Date(end),
+            name: courseTitle.textContent.trim(),
+            classType,
+            classSec,
+            prof: profOrder[i],
+            loc: roomOrder[i][ti][tbi], // every time has a corresponding room
+            ...legendTimeToRecur(tblock, Number(year)),
+            // legendTimeToRecur returns an object of type { freq: { ... }, begin?: Date, end?: Date }
+          }),
+        );
+      });
     });
   }
-
   console.log(data);
 
   return data;
